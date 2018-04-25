09b1f894e481b938f788bd5a4bb26a673f52bdb9
Yilmaz Uksal <yilmazu@intellis.io>
2018-04-25 16:01:46 -0400

    Added More Cool Stuff

--- Test/MainWindow.xaml.cs
+++ Test/MainWindow.xaml.cs
@@ -42,11 +42,34 @@ namespace Test
     {
         DispatcherTimer dispatcherTimer = new DispatcherTimer();
         Entities en = new Entities();
+        Dictionary<string, Table> tables = new Dictionary<string, Table>();
 
         Modes mode = Modes.None;
         Button s = null;
         Button selected = null;
         int id = 0;
+
+        enum Shape
+        {
+            Circle,
+            Rectangle
+        }
+
+        class Table
+        {
+            public string Id { get; set; }
+            public Shape Shape { get; set; }
+            public string Text { get; set; }
+            public int RotateAngle { get; set; }
+            public double Width { get; set; }
+            public double Height { get; set; }
+            public double X { get; set; }
+            public double Y { get; set; }
+            public int ScaleX { get; set; }
+            public int ScaleY { get; set; }
+            public SolidColorBrush color { get; set; }
+        }
+        
 
         class Person : INotifyPropertyChanged
         {
@@ -124,34 +147,99 @@ namespace Test
             return sbInput.ToString();
         }
 
+        bool leftDown = false;
+
         private void Re_MouseDown(object sender, MouseButtonEventArgs e)
         {
+            leftDown = true;
+            if (selected != null)
+            {
+                Table t = GetSelectedTable();
+                if (t.Shape == Shape.Rectangle)
+                {
+                    selected.Template = ButtonFactory.GetRectangle(t.color);
+                }
+                else
+                {
+                    selected.Template = ButtonFactory.GetCircle(t.color);
+                }
+            }
+            foreach (Button shape in C.Children)
+            {
+                Rect r = new Rect(new Point(VisualTreeHelper.GetOffset(shape).X, VisualTreeHelper.GetOffset(shape).Y), new Size(shape.Width, shape.Height));
+                if (r.Contains(e.GetPosition(C)))
+                {
+                    shape.BorderBrush = Brushes.Blue;
+                    shape.Background = Brushes.Blue;
+                    s = shape;
+                    if (mode == Modes.Select)
+                    {
+                        selected = shape;
+                        Table tb = tables[selected.Name];
+                        if (tb.Shape == Shape.Rectangle)
+                        {
+                            shape.Template = ButtonFactory.GetRectangle(Brushes.LightGray);
+                        }
+                        else
+                        {
+                            shape.Template = ButtonFactory.GetCircle(Brushes.LightGray);
+                        }
+                    }
+                }
+                else
+                {
+                    shape.BorderBrush = Brushes.Red;
+                }
 
+            }
         }
 
         private void Re_MouseMove(object sender, MouseEventArgs e)
         {
-            if (mode == Modes.Drag)
+            if (selected != null && leftDown)
             {
-                Canvas.SetLeft(s, e.GetPosition(C).X - s.Width / 2);
-                Canvas.SetTop(s, e.GetPosition(C).Y - s.Height / 2);
+                Table t = GetSelectedTable();
+                double newX = e.GetPosition(C).X - s.Width / 2;
+                double newY = e.GetPosition(C).Y - s.Height / 2;
+                t.X = newX;
+                t.Y = newY;
+                Canvas.SetLeft(s, t.X);
+                Canvas.SetTop(s, t.Y);
             }
         }
 
-        private void CreateTable(Table type)
+        private void CreateTable(Shape type)
         {
+            Table t = new Table()
+            {
+                Id = "S" + id,
+                Text = id.ToString(),
+                Width = 40,
+                Height = 40,
+                X = 50,
+                Y = 50,
+                RotateAngle = 0,
+                Shape = type,
+                ScaleX = 0,
+                ScaleY = 0,
+                color = Brushes.LightGreen
+            };
+
+            tables.Add(t.Id, t);
+
             Button el = new Button();
-            el.Content = id;
+            el.Content =t.Text;
+            el.Name = t.Id;
             id++;
-            el.Width = 40;
-            el.Height = 40;
-            Canvas.SetLeft(el, 50);
-            Canvas.SetTop(el, 50);
+            el.Width = t.Width + t.ScaleX;
+            el.Height = t.Height + t.ScaleY;
+            Canvas.SetLeft(el, t.X);
+            Canvas.SetTop(el, t.Y);
             el.BorderBrush = Brushes.Red;
             //Style s = this.FindResource(type == Table.Round ? "Circle" : "Rectangle") as Style;
 
             //el.Style = this.FindResource(type == Table.Round ? "Circle" : "Rectangle") as Style;
-            if (type == Table.Round)
+            if (type == Shape.Circle)
             {
                 el.Template = ButtonFactory.GetCircle(Brushes.LightGreen);
             }
@@ -165,6 +253,11 @@ namespace Test
         }
 
         int angle = 45;
+
+        private Table GetSelectedTable()
+        {
+            return tables[selected.Name];
+        }
 
         private void Button_Click(object sender, RoutedEventArgs e)
         {
@@ -182,6 +275,15 @@ namespace Test
             else if (b.Content.ToString() == "Unselect")
             {
                 mode = Modes.None;
+                Table t = GetSelectedTable();
+                if (t.Shape == Shape.Rectangle)
+                {
+                    selected.Template = ButtonFactory.GetRectangle(t.color);
+                }
+                else
+                {
+                    selected.Template = ButtonFactory.GetCircle(t.color);
+                }
                 selected = null;
                 b.Content = "Select";
             }
@@ -189,10 +291,12 @@ namespace Test
             {
                 if(selected != null)
                 {
+                    Table t = GetSelectedTable();
                     RotateTransform rotateTransform = new RotateTransform(angle);
-                    rotateTransform.CenterX = selected.Width / 2;
-                    rotateTransform.CenterY = selected.Height / 2;
-                    angle += 45;
+                    rotateTransform.CenterX = t.Width / 2;
+                    rotateTransform.CenterY = t.Height / 2;
+                    t.RotateAngle += 45;
+                    angle = t.RotateAngle;                    
                     selected.RenderTransform = rotateTransform;
                 }
             }
@@ -200,24 +304,28 @@ namespace Test
             {
                 if(selected != null)
                 {
-                    selected.Width += 10;
-                    selected.UpdateLayout();
+                    Table t = GetSelectedTable();
+                    t.ScaleX += 10;
+                    selected.Width = t.Width + t.ScaleX;                    
                 }
             }
             else if (b.Content.ToString() == "ScaleY")
             {
-                ((List)CB.SelectedItem).Name = "TEST";
+                Table t = GetSelectedTable();
+                t.ScaleY += 10;
+                selected.Height = t.Height + t.ScaleY;
+                //((List)CB.SelectedItem).Name = "TEST";
                 //people.Add(new Person() { Name = "J" });
                 //people[0].Name = "Z";
                 //CB.Items.Refresh();
             }
             else if(b.Content.ToString() == "Circle")
             {
-                CreateTable(Table.Round);
+                CreateTable(Shape.Circle);
             }
             else if (b.Content.ToString() == "Rectangle")
             {
-                CreateTable(Table.Square);
+                CreateTable(Shape.Rectangle);
             }
             else
             {
@@ -225,30 +333,6 @@ namespace Test
             }
         }
 
-        private void C_MouseDown(object sender, MouseButtonEventArgs e)
-        {
-            if(mode == Modes.Select)
-            {
-                foreach (Button shape in C.Children)
-                {
-                    Rect r = new Rect(new Point(Canvas.GetLeft(shape), Canvas.GetTop(shape)), new Size(shape.Width, shape.Height));
-                    if (r.Contains(e.GetPosition(C)))
-                    {
-                        shape.BorderBrush = new SolidColorBrush(Colors.Blue);
-                        s = shape;
-                    }
-                    else
-                    {
-                        shape.BorderBrush = new SolidColorBrush(Colors.Red);
-                    }
-
-                }
-
-                //HitTestResult result = VisualTreeHelper.HitTest(Re, e.GetPosition(C));
-                //((Shape)result.VisualHit).Stroke = new SolidColorBrush(Colors.Blue);
-                //s = ((Shape)result.VisualHit);
-            }
-        }
 
         private void C_MouseRightButtonDown(object sender, MouseButtonEventArgs e)
         {
@@ -293,6 +377,12 @@ namespace Test
 
         private void C_TouchUp(object sender, TouchEventArgs e)
         {
+            s = null;
+        }
+
+        private void C_MouseLeftButtonUp(object sender, MouseButtonEventArgs e)
+        {
+            leftDown = false;
             s = null;
         }
     }
